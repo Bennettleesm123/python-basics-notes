427bfe1095d6b4f100471d32518686f9d8189990
Bennettleesm123 <bennettleesm@gmail.com>
2025-08-31 18:24:40 -0400

    Update robot_move.py with more detailed descriptions for understanding

--- robot_move.py
+++ robot_move.py
@@ -1,6 +1,14 @@
 """
 robot_move.py
 Tiny simulation: robot moves in a 2D grid with commands.
+Start position: [0, 0].
+A function move(cmd) takes a command.
+If it’s "up", increase y.
+If it’s "down", decrease y.
+"left" decreases x.
+"right" increases x.
+After each move, print the new position.
+The demo makes the robot move up, up, left, down.
 """
 
 position = [0, 0]  # x, y
